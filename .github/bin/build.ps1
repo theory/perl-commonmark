173a239132bf5d96ca21154062000dfe700f14e2
David E. Wheeler <david@justatheory.com>
2023-09-11 11:13:29 -0400

    DLL in bin dir

--- .github/bin/build.ps1
+++ .github/bin/build.ps1
@@ -13,9 +13,10 @@ echo "Building ${BASENAME} in ${PREFIX}";
 C:\msys64\usr\bin\wget.exe -q "https://github.com/jgm/cmark/archive/${VERSION}.tar.gz"
 tar -zxf "${VERSION}.tar.gz"
 cd "${BASENAME}"
-nmake /nologo /f Makefile.nmake install
+nmake /nologo /f Makefile.nmake INSTALLDIR="${PREFIX}" install
 cd ..
 
 echo "Building and testing CommonMark"
-perl Makefile.PL
+$env:PATH += ";${PREFIX}/bin"
+perl Makefile.PL INC="-I${PREFIX}/include" LIBS="-L${PREFIX}/lib -lcmark"
 make test
